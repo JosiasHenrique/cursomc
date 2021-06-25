9eaf00bd7831c2fe7f9cf19b27993d0c66ea0a2d
JosiasHenrique <joystickhenrii@gmail.com>
2021-06-24 22:40:38 -0300

    Projeto criado

---
+++ src/main/java/com/josiashenrique/cursomc/CursmomcApplication.java
@@ -0,0 +1,13 @@
+package com.josiashenrique.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursmomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursmomcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/josiashenrique/cursomc/CursmomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josiashenrique.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursmomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
