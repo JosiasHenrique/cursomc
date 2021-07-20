2f3f987c4322f465dbe03bb7935376704987969d
Josias Henrique <joystickhenrii@gmail.com>
2021-07-20 14:57:19 -0300

    Validação sintatica com Bean Validation

--- src/main/java/com/josiashenrique/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/josiashenrique/cursomc/dto/CategoriaDTO.java
@@ -2,23 +2,28 @@ package com.josiashenrique.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.josiashenrique.cursomc.domain.Categoria;
 
 public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
-	private String nome;
 	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5, max=80, message="O tamanho deve ser entre 5 e 80 caracteres")
+	private String nome;
+
 	public CategoriaDTO() {
-		
 	}
 	
 	public CategoriaDTO(Categoria obj) {
-		 id = obj.getId();
-		 nome = obj.getNome();
+		id = obj.getId();
+		nome = obj.getNome();
 	}
-	
 
 	public Integer getId() {
 		return id;
@@ -34,6 +39,5 @@ public class CategoriaDTO implements Serializable {
 
 	public void setNome(String nome) {
 		this.nome = nome;
-	}
-
+	}	
 }
--- src/main/java/com/josiashenrique/cursomc/services/CategoriaService.java
+++ src/main/java/com/josiashenrique/cursomc/services/CategoriaService.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.josiashenrique.cursomc.domain.Categoria;
+import com.josiashenrique.cursomc.dto.CategoriaDTO;
 import com.josiashenrique.cursomc.repositories.CategoriaRepository;
 import com.josiashenrique.cursomc.services.exceptions.DataIntegrityException;
 import com.josiashenrique.cursomc.services.exceptions.ObjectNotFoundException;
@@ -58,4 +59,9 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
 	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome()); 
+	}
+	
 }
