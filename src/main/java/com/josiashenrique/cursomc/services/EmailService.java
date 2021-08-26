3909a8220cc7c7d591c394ab5c0b59c61c6adaa7
Josias Henrique <joystickhenrii@gmail.com>
2021-08-26 17:24:21 -0300

    Email HTML

--- src/main/java/com/josiashenrique/cursomc/services/EmailService.java
+++ src/main/java/com/josiashenrique/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.josiashenrique.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.josiashenrique.cursomc.domain.Pedido;
@@ -7,6 +9,10 @@ import com.josiashenrique.cursomc.domain.Pedido;
 public interface EmailService {
 
 	void sendOrderConfirmationEmail(Pedido obj);
-	
+
 	void sendEmail(SimpleMailMessage msg);
+
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+
+	void sendHtmlEmail(MimeMessage msg);
 }
--- src/main/java/com/josiashenrique/cursomc/services/MockEmailService.java
+++ src/main/java/com/josiashenrique/cursomc/services/MockEmailService.java
@@ -1,9 +1,10 @@
 package com.josiashenrique.cursomc.services;
 
-import org.slf4j.Logger;
-import org.springframework.mail.SimpleMailMessage;
+import javax.mail.internet.MimeMessage;
 
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.mail.SimpleMailMessage;
 
 public class MockEmailService extends AbstractEmailService{
 
@@ -12,6 +13,14 @@ public class MockEmailService extends AbstractEmailService{
 	@Override
 	public void sendEmail(SimpleMailMessage msg) {
 		LOG.info("Simulando envio de email...");
+		LOG.info(msg.toString());
+		LOG.info("Email enviado");
+		
+	}
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("Simulando envio de email HTML...");
 		LOG.info(msg.toString());
 		LOG.info("Email enviado");
 		
