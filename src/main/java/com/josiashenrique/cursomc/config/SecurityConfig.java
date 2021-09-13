1af022612aca7609f07009685a83d793206403c1
Josias Henrique <joystickhenrii@gmail.com>
2021-09-13 19:53:26 -0300

    Autorizando endpoints para perfis específicos

--- src/main/java/com/josiashenrique/cursomc/config/SecurityConfig.java
+++ src/main/java/com/josiashenrique/cursomc/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -25,6 +26,7 @@ import com.josiashenrique.cursomc.security.JWTUtil;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
@@ -36,19 +38,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private JWTUtil jwtUtil;
 	
-	//Lista de endpoints que será permitido sem autenticação.
 	private static final String[] PUBLIC_MATCHERS = {
 			"/h2-console/**"
 	};
 	
-	//Lista de endpoints que será permitido sem autenticação.
 		private static final String[] PUBLIC_MATCHERS_GET = {
 				"/produtos/**",
-				"/categorias/**",
+				"/categorias/**"
+		};
+		
+		private static final String[] PUBLIC_MATCHERS_POST = {
 				"/clientes/**"
 		};
 	
-	//Método para falar quais os endpoints que nao precisa de autenticação e exigir auth do resto.
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		
@@ -58,9 +60,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		
 		http.cors().and().csrf().disable();
 		http.authorizeRequests()
-		.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
-		.antMatchers(PUBLIC_MATCHERS).permitAll()
-		.anyRequest().authenticated();
+			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+			.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
+			.antMatchers(PUBLIC_MATCHERS).permitAll()
+			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
 		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
