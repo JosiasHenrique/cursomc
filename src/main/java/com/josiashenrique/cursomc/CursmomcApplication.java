bd60c443239a55ad448ed5428f3568b2318e82c1
Josias Henrique <joystickhenrii@gmail.com>
2021-09-14 15:34:22 -0300

    Enviando imagem via endpoint

--- src/main/java/com/josiashenrique/cursomc/CursmomcApplication.java
+++ src/main/java/com/josiashenrique/cursomc/CursmomcApplication.java
@@ -1,17 +1,11 @@
 package com.josiashenrique.cursomc;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.josiashenrique.cursomc.services.S3Service;
-
 @SpringBootApplication
 public class CursmomcApplication implements CommandLineRunner {
-
-	@Autowired
-	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursmomcApplication.class, args);
@@ -19,7 +13,6 @@ public class CursmomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-
-		s3Service.uploadFile("C:\\Users\\Jooj\\Pictures\\teste\\jooj.jpg");
+		
 	}
 }
--- src/main/java/com/josiashenrique/cursomc/services/ClienteService.java
+++ src/main/java/com/josiashenrique/cursomc/services/ClienteService.java
@@ -1,6 +1,7 @@
 package com.josiashenrique.cursomc.services;
 
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,6 +14,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.josiashenrique.cursomc.domain.Cidade;
 import com.josiashenrique.cursomc.domain.Cliente;
@@ -40,6 +42,9 @@ public class ClienteService {
 	
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	public Cliente find(Integer id) {
 		
@@ -111,4 +116,7 @@ public class ClienteService {
 		newObj.setEmail(obj.getEmail());
 	}
 	
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return s3Service.uploadFile(multipartFile);
+	}
 }
